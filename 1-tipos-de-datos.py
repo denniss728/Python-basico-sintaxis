8880d30431995f31b72a964f8bf625ec39feb90c
denniss728 <bernabedennis27@gmail.com>
2026-02-25 12:31:11 -0500

    first commit

---
+++ 1-tipos-de-datos.py
@@ -0,0 +1,23 @@
+#YUNIOR DENNIS
+#Tipos de datos en python
+#1. Tipo de dato númerico 
+edad = 25
+promedio = 19
+número = 50
+
+
+#2. Tipo de dato decimal (float)
+peso = 75.50
+estatura = 170.30
+temperatura = 14.70
+
+#3. tipo de dato texto (str)
+letra = 'A'
+nombre = 'Yunior'
+Apellido = 'Bernabe'
+
+#4. tipo de dato booleanos (bool)
+es_mayor_de_edad = True
+puede_volar = False
+
+
---
+++ 2-operadores.py
@@ -0,0 +1,46 @@
+#YUNIOR DENNIS
+
+#Operadores en python
+
+#1. Operadores aritméticos 
+suma = 5 + 5 
+resta = 10 - 6
+multiplicación = 5*5 
+División = 10 / 2
+modulo = 10 % 6
+Exponente = 10 ** 2 
+
+print(suma)
+print(resta)
+print(multiplicación)
+print(División)
+print(modulo)
+print(Exponente)
+
+#2. Operadores de comparación
+print( 5 == 5 ) # Igual a 
+print( 5 != 5 ) # Diferente de 
+print( 10 > 5 ) # Mayor que
+print( 10 < 5 ) # Menor que 
+print( 10 >= 5 ) # Mayor igual que
+print( 10 <= 5 ) # Menor igual que 
+
+#3. Operadores Lógicos 
+v = True
+f = False
+#3.1. and (y)
+print('-------------------------AND')
+print(v and v)
+print(v and f)
+print(f and v)
+print(f and f)
+#3.2. or (o)
+print('-------------------------OR')
+print(v and v)
+print(v and f)
+print(f and v)
+print(f and f)
+#3.3. not (negacion)
+print('-------------------------NOT')
+print( not v)
+print( not f)
